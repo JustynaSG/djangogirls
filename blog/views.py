1aa77d4121ddaee5c260a8648d5b472a1e26dfab
Justyna <justyna@gazeta.pl>
2015-07-11 17:42:07 +0200

    sukces

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+
+    p=Post.objects.order_by('title')
+    return render(request, 'blog/post_list.html', {"post":p})
